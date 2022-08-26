266fe85b6bbcf82a24d903e4ac1b0c73cfc35d79
qaptoR <rocco.ruscitti@outlook.com>
2022-08-26 06:08:16 -0700

    Implementation of new Camera3D type: Oblique allows the specification of a world plane for near clipping; applies matrix manipulation to generate oblique near plane.

--- servers/rendering/renderer_scene_render.h
+++ servers/rendering/renderer_scene_render.h
@@ -312,6 +312,7 @@ public:
 		// Main/center projection
 		Transform3D main_transform;
 		Projection main_projection;
+		Projection shadow_projection;
 
 		Transform3D view_offset[RendererSceneRender::MAX_RENDER_VIEWS];
 		Projection view_projection[RendererSceneRender::MAX_RENDER_VIEWS];
