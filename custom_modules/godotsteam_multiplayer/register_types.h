a9ce00cd8fa41fcc5aa0d3d34ae5e9766ab3a5d8
Riordan <riordancallil@outlook.com>
2024-09-07 07:36:49 +1000

    godotsteam_multiplayer

---
+++ custom_modules/godotsteam_multiplayer/register_types.h
@@ -0,0 +1,9 @@
+#ifndef GODOTSTEAMMULTIPLAYER_REGISTER_TYPES_H
+#define GODOTSTEAMMULTIPLAYER_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+void initialize_godotsteam_multiplayer_module(ModuleInitializationLevel level);
+void uninitialize_godotsteam_multiplayer_module(ModuleInitializationLevel level);
+
+#endif
